dbdfd7497772fc3ded12de2f132661cbc357fd02
xien.#DDoS <57267004+ItsNiceDiamond@users.noreply.github.com>
2020-08-29 20:44:18 +0300

    Update Sm@le.py

--- Sm@le.py
+++ Sm@le.py
@@ -2,7 +2,7 @@ import smtplib
 import colorama
 from colorama import init, Fore, Back
 from os import system
-clear
+echo ('clear')
 print('   ======================  ')
 print('     Sm@le - email bute               ')
 print('   ======================  ')
